1d7850364de93ed68e2b65466af651e468c5cdcb
Leonardo Quintero <lleo5301@gmail.com>
2015-01-22 09:21:34 -0500

    Add boilerplate js script

---
+++ index.js
@@ -0,0 +1,33 @@
+///boilerplate
+
+//dependencies
+var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+
+//configure body parser
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+//set port
+var port = process.env.PORT || 5300;
+
+//set routes for the api
+var router = express.Router();
+
+//test
+router.get('/', function(req, res){
+  res.json({message:"This api works"});
+
+});
+
+//to-do add real routes
+
+
+///register routes
+app.use('/api', router);
+
+//lets start the server
+//=========================================================
+app.listen(port);
+console.log('Website on port : ' + port);
