3e6ce68315ff460de09fedc309d13d94cd0fc588
Leonardo Quintero <lleo5301@gmail.com>
2015-01-22 10:22:15 -0500

    add mongo connection

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var app     = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/rb')
 
 //configure body parser
 app.use(bodyParser.urlencoded({ extended: true}));
