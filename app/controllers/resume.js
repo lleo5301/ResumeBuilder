8d97c4cd770c262f6002b3c68c39a99dff0d175e
Leonardo Quintero <lleo5301@gmail.com>
2015-01-23 16:30:51 -0500

    Add post method

--- app/controllers/resume.js
+++ app/controllers/resume.js
@@ -1,23 +1,43 @@
 angular.module('resumify')
 
 .controller('resume',['$scope','$http', function($scope, $http){
-  $scope.resumes = function()
-{
-  $http.get("/api/resumes")
-  .success(function(response){
-    $scope.data = response;
-    console.log($scope.data);
-  });
-};
+
+  //empty resume
+  $scope.resume = {};
+  //empty address
+  $scope.Address={};
+
+  $scope.resumes = function(){
+    $http.get("/api/resumes")
+    .success(function(response){
+      $scope.data = response;
+      console.log($scope.data);
+    });
+  };
 
   $scope.skills=[];
 
   $scope.addSkill=function(){
     if(event.keyCode == 13 && $scope.skill){
-      $scope.skills.push($scope.skill);
+      $scope.resume.Skills.push($scope.skill);
       $scope.skill = "";
     }
   };
+
+  $scope.save=function(){
+      //add address to resume
+      $scope.resume.Address = $scope.Address;
+      $scope.resume.Skills = $scope.skills;
+      $http.post('/api/resumes', $scope.resume)
+      .success(function(data){
+        console.log(data);
+        $scope.resume=data;
+      })
+      .error(function(data){
+        console.log('Error:' + data);
+      });
+  };
+
 
 
 }])
