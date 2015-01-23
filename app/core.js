baa0f9d0360890d66a5ffedad4fdcfe1190c148d
Leonardo Quintero <lleo5301@gmail.com>
2015-01-23 11:26:59 -0500

    Start angularjs part of app

---
+++ app/core.js
@@ -0,0 +1,11 @@
+var resumify = angular.module('resumify', ['ngRoute']);
+
+resumify.config(['$routeProvider', '$httpProvider',function($routeProvider, $httpProvider){
+
+  $routeProvider.when("/", {
+    controller="home",
+    });
+  
+
+
+}]);
