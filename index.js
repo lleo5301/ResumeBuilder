b5f34636b1cc69794fee67a1a162aad756d8400d
Leonardo Quintero <lleo5301@gmail.com>
2015-01-23 09:42:07 -0500

    Add home path

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ router.use(function(req,res,next){
 });
 
 //test
-router.get('/', function(req, res){
+router.get('/home', function(req, res){
   res.sendFile('./views/index.html');
   //res.json({message:"This api works"});
 
