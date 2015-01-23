a839c47f6fc06948372f383491db719c5296d729
Leonardo Quintero <lleo5301@gmail.com>
2015-01-23 09:45:54 -0500

    Home route = views/index.html

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ router.use(function(req,res,next){
 
 //test
 router.get('/', function(req, res){
-  //res.sendFile('./views/index.html');
+  res.sendFile('/views/index.html');
   //res.json({message:"This api works"});
-  res.send('hello world');
+  //res.send('hello world');
 });
 
 //resume route
