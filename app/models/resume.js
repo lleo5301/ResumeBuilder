9ce3986ed2c68afd96e5789beba5b69bed608b5f
Leonardo Quintero <lleo5301@gmail.com>
2015-01-24 13:41:42 -0500

    add experience and description

--- app/models/resume.js
+++ app/models/resume.js
@@ -1,25 +1,39 @@
 //app/models/resume.js
 
 var mongoose = require('mongoose');
+//var Resume = require('./app/models/resume');
 var Schema = mongoose.Schema;
 
 // var Skills = new Schema({
 //    List = [String]
 // });
 
+
+var Experience = new Schema({
+    Name:String,
+    Title:String,
+    Address:{lineOne: String,
+      lineTwo: String,
+      state: String,
+      city: String,
+      zip: Number},
+    Duties:[String]
+
+});
+
 var ResumeSchema = new Schema({
     Name: String,
     Title: String,
     Email: String,
     Phone: Number,
-    Address:{
-      lineOne: String,
+    Address:{lineOne: String,
       lineTwo: String,
       state: String,
       city: String,
-      zip: Number
-    },
-    Skills: [String]
+      zip: Number},
+    Summary:String,
+    Skills: [String],
+    Experience:[Experience]
 
 });
 
